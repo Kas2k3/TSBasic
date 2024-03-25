82ab985b3904af195ff7ae566d5a5705bbcb2773
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2024-03-25 11:54:40 +0700

    keyword type

---
+++ Bai1_TypeAnnotations/lesson1.js
@@ -0,0 +1,6 @@
+let count = 1;
+count = 'name';
+console.log(">>> check count: ", count)
+
+let names = ['kas', 'TypeScript']
+name.push(25)
---
+++ Bai1_TypeAnnotations/lesson1.ts
@@ -0,0 +1,8 @@
+// let count2 : number = 1; //annotation
+
+// console.log(">>> check count2: ", count2)
+
+let names2: string[] = ['kas', 'TypeScript']
+names2.push("25")
+
+console.log(">>> check name 2: ", names2)
