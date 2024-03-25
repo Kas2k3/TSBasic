356c981dc64df4c95108f28370f252029c277fa9
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2024-03-25 12:40:57 +0700

    Kieu du lieu object

---
+++ Bai6_ObjectType/lesson6.js
@@ -0,0 +1,7 @@
+let mine = {};
+mine = { name: 'kas' };
+
+//truoc do khong he co thuoc tinh address
+mine.address = "kas";
+
+console.log(">>> check mine: ", mine)
---
+++ Bai6_ObjectType/lesson6.ts
@@ -0,0 +1,12 @@
+// let person: object;
+// let person: {};
+// person = {name: 'kas'};
+
+let pro1: {
+    name: string,
+    age: number
+} = {
+    name: "kas", //string
+    age: 21 //number
+}
+
