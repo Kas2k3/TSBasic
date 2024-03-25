33a2f1cfd468d1ce7900397366418a3d06863358
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2024-03-25 12:11:19 +0700

    Tu dong gan kieu du lieu

---
+++ Bai2_TypeInference/lesson2.js
@@ -0,0 +1,3 @@
+let count0 = 10;
+count0 = 'kas';
+console.log(">>>check count 0 = ", count0) 
---
+++ Bai2_TypeInference/lesson2.ts
@@ -0,0 +1,8 @@
+let count = "havinhphuoc";
+// count = 'kas';
+console.log(">>>check count 0 = ", count)
+
+let test = ['kas', 'havinhphuoc', 'typescript', 96];
+test.push(25);
+
+//Dung tu dong gan khi muon no thay doi kieu du lieu theo thuat toan cua no
