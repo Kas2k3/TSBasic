2da0f2853ddeac4a8ec7a8ad9e7a8e47e7951d74
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2024-03-25 12:31:14 +0700

    kieu du lieu boolean

---
+++ Bai5_BooleanType/lesson5.ts
@@ -0,0 +1,7 @@
+let status1: boolean = false;
+let check1: boolean = true;
+
+//chuoi tong la false, chuoi khong rong la false
+let pro: boolean = Boolean("kas");
+console.log(">>> check pro: ", pro)
+//Phan biet boolean va Boolean
