ec0acc6bb82ed075686d4acf9d9e780507bf07ca
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2024-03-27 08:11:54 +0700

    Kieu du lieu void

---
+++ Bai11_VoidType/lesson16.ts
@@ -0,0 +1,8 @@
+const sum = (a: number, b: number): number => {
+    return a + b;
+}
+
+//voi dung de bao hieu 1 function khong tra ve gia tri gi
+const handleLogs = (message: string): void => {
+    console.log(">>> message: ", message)
+}
