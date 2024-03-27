15007c339ce6e1cd24f9e4f387777f1e4f86e900
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2024-03-27 09:25:52 +0700

    break trong ts

---
+++ Chapter2/Bai20_Break/lesson20.ts
@@ -0,0 +1,27 @@
+let products = [
+    { name: 'phone', price: 700 },
+    { name: 'tablet', price: 900 },
+    { name: 'laptop', price: 1200 }
+];
+
+for (let i = 0; i < products.length; i++) {
+    if (products[i].price == 900) {
+        break;
+    }
+}
+
+let discount = 0;
+let product = products[1];
+switch (product.name) {
+    case 'phone':
+        discount = 5;
+        break;
+    case 'tablet':
+        discount = 10;
+        break;
+    case 'laptop':
+        discount = 15;
+        break;
+    default:
+        discount = 100;
+}
