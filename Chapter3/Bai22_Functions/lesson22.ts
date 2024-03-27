c514a926f2b793524a4e626f75dadc60317223f8
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2024-03-27 09:39:48 +0700

    function trong ts

---
+++ Chapter3/Bai22_Functions/lesson22.ts
@@ -0,0 +1,10 @@
+function sum(a: number, b: number) {
+    return a + b;
+}
+console.log(">>> check sum: ", sum(6, 9));
+
+//anonymous function
+const sum2 = (a: number, b: number) => {
+    return a + b;
+}
+console.log(">>> check sum2: ", sum2(6, 9));
